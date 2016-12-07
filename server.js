064d66e49de748f50eb4cada85e62502d404cc4b
Clemens <asdfsadf>
2016-12-07 18:44:31 +0100

    added navbar in header

--- server.js
+++ server.js
@@ -33,7 +33,14 @@ app.set("view engine", "ejs");
 
 // Landing page
 app.get("/", function(req, res){
-    res.render("home");
+    var url = "https://api.themoviedb.org/3/tv/popular?api_key=b0b76e71443fa8e798d4ee211e73e3e1&language=en-US&page=1";
+
+    request(url, function(error, response, body){
+        if(!error && response.statusCode == 200){
+            var series = JSON.parse(body);
+            res.render("home", {series:series});
+        }
+    });
 });
 
 
